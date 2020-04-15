66b54077a7c34aa7e39b7477f037813490745e00
Jason Cook <the.root.user@gmail.com>
2020-04-14 21:41:36 -0400

    Scaffold redux

---
+++ api/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { initialState } from '../state';
+
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        default: {
+            return state
+        }
+    }
+
+}
---
+++ api/src/state/index.js
@@ -0,0 +1,11 @@
+export const initialState = {
+    ask: '977.59',
+    bid: '977.35',
+    last: '977.65',
+    volume: {
+        BTC: '2210.505328803',
+        USD: '2135477.463379586263',
+        timestamp: 1483018200000,
+    },
+    isLoading: false
+};
